1981146e841935e4d212f1ac4d064f6f17259959
myy-s <1054165055@qq.com>
2020-04-14 02:13:40 +0800

    增加icon组件,使用svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>|
-    <router-link to="/labels">标签</router-link>|
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <Icon name="money" />记账
+    </router-link>|
+    <router-link to="/labels">
+      <Icon name="label" />标签
+    </router-link>|
+    <router-link to="/statistics">
+      <Icon name="statistics" />统计
+    </router-link>
   </div>
 </template>
 
@@ -12,5 +18,4 @@ export default {
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
