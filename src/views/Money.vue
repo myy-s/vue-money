04c4a838a4bc0243c8d2d530fb9674d811603082
myy-s <1054165055@qq.com>
2020-04-21 21:39:31 +0800

    样式模块化

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,13 +1,25 @@
 <template>
-  <Layout>
-    <p>Money</p>
+  <Layout class-prefix="layout">
+    <NumberPad />
+    <Types />
+    <Notes />
+    <Tags />
   </Layout>
 </template>
 
 <script lang="ts">
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import Notes from "@/components/Money/Notes.vue";
+import Tags from "@/components/Money/Tags.vue";
 export default {
-  name: "Money"
+  name: "Money",
+  components: { Tags, Notes, Types, NumberPad }
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
 </style>
