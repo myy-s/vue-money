bf758a75735a4a7653b77b88313306c554d4fac6
myy-s <1054165055@qq.com>
2020-04-28 01:18:27 +0800

    封装taglist增删改查

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -22,4 +22,11 @@ type TagListModel = {
 
 interface Window {
   tagList: Tag[];
+  createTag: (name: string) => void;
+  findTag: (id: string) => Tag | undefined;
+  removeTag: (id: string) => boolean;
+  updateTag: (
+    id: string,
+    name: string
+  ) => "success" | "not found" | "duplicated";
 }
