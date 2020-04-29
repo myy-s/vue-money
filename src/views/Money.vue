15f772033a2fbddf0fbd2b07cd935d5de5aaf39a
myy-s <1054165055@qq.com>
2020-04-29 21:32:02 +0800

    修改样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -11,7 +11,6 @@
 
 <script lang="ts">
 import NumberPad from "@/components/Money/NumberPad.vue";
-import Types from "@/components/Money/Types.vue";
 import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
 import Vue from "vue";
@@ -20,7 +19,7 @@ import Tabs from "@/components/Tabs.vue";
 import recordTypeList from "@/constants/recordTypeList";
 
 @Component({
-  components: { Tabs, Tags, FormItem, Types, NumberPad }
+  components: { Tabs, Tags, FormItem, NumberPad }
 })
 export default class Money extends Vue {
   get recordList() {
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -11,14 +11,13 @@
 </template>
 
 <script lang="ts">
-import Types from "@/components/Money/Types.vue";
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Tabs from "@/components/Tabs.vue";
 import intervalList from "@/constants/intervalList";
 import recordTypeList from "@/constants/recordTypeList";
 @Component({
-  components: { Tabs, Types }
+  components: { Tabs }
 })
 export default class Statistics extends Vue {
   type = "-";
@@ -37,5 +36,8 @@ export default class Statistics extends Vue {
       display: none;
     }
   }
+}
+::v-deep .interval-tabs-item {
+  height: 48px;
 }
 </style>
