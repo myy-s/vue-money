9945536c0510074f5d7cf9973492bb7cbfdfa51d
myy-s <1054165055@qq.com>
2020-05-02 03:04:18 +0800

    修改二维码路劲2

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ window.onload = function() {
 if (document.documentElement.clientWidth > 500) {
   window.alert("请使用手机打开本页面，以保证浏览效果");
   const img = document.createElement("img");
-  img.src = "/dist/qrcode.png";
+  img.src = "./qrcode.png";
 
   img.style.position = "fixed";
   img.style.top = "50%";
